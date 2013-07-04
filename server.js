a4be3ed72e51f1b67417a4b3ceb3c79248d52e55
Chris Kim <cj3kim@gmail.com>
2013-07-04 16:14:39 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express')
+  , consolidate = require('consolidate')
+  , path = require('path')
+  , app = express();
+
+app.engine('html', consolidate.mustache);
+app.set('view engine', 'html');
+app.set('views', __dirname + "/views");
+
+app.use(express.logger());
+app.use("/css", express.static(path.join(__dirname, "/assets/css")));
+app.use("/js", express.static(path.join(__dirname, "/assets/js")));
+
+app.get('/', function(req, res) {
+  res.render('index', {title: "barge"});
+});
+
+app.get('/upload', function(req, res) {
+  res.send("<h1> File Upload Pagge </h1>");
+});
+
+app.listen(3000);
+
+console.log("Express server started on port 3000");
