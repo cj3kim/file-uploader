5defc5533b0d1658115f1aa8d94a55590a0d4dbe
Chris Kim <cj3kim@gmail.com>
2013-07-19 02:31:31 -0700

    Users can drop images into the file upload area without automatic browser redirection

--- assets/js/upload.js
+++ assets/js/upload.js
@@ -2,19 +2,35 @@ function handleFileDrop(e) {
   e.preventDefault();
   e.stopPropagation();
 
+  files = e.dataTransfer.files;
+
+  return false;
+};
+
+function handleDragOver(e) {
+  console.log('over');
+  e.preventDefault();
+  e.stopPropagation();
+
+  return false;
 };
 
 function handleDragEnter(e) {
+  console.log('enter');
   e.preventDefault();
   e.stopPropagation();
 
   this.classList.add('dotted');
+
+  return false;
 };
 
 function handleDragLeave(e) {
+  console.log('leave');
   e.preventDefault();
   e.stopPropagation();
 
   this.classList.remove('dotted');
+  return false;
 };
 
